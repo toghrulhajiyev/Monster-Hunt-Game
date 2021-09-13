9f9f521f400e0743a8a1f38e8a7d52dfc41f2a6c
toghrulhajiyev <togrul.haciyev24@gmail.com>
2021-09-14 00:01:59 +0400

    Listing logs done

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsOn: false,
+        logs: [],
     },
     methods: {
         newGame: function() {
@@ -12,25 +13,36 @@ new Vue({
         attack: function() {
             let attackPoint = Math.ceil(Math.random() * 10);
             this.monsterHealth -= attackPoint;
+            this.addToLog({turn: 'Player', text: 'Player attack('+ attackPoint +')'});
             this.monsterAttack();
         },
         specialAttack: function() {
             let attackPoint = Math.ceil(Math.random() * 25);
             this.monsterHealth -= attackPoint;
+            this.addToLog({turn: 'Player', text: 'Special player attack('+ attackPoint + ')'});
+
             this.monsterAttack();
         },
         healUp: function() {
             let healPoint = Math.ceil(Math.random() * 20);
             this.playerHealth += healPoint;
+            this.addToLog({turn: 'Player', text: 'Player healed up('+ healPoint +')'});
+
             this.monsterAttack();
         },
         giveUp: function() {
             this.playerHealth = 0;
+            this.addToLog({turn: 'Player', text: 'Player gave up'});
         },
         monsterAttack: function() {
             let attackPoint = Math.ceil(Math.random() * 15);
             this.playerHealth -= attackPoint;
-        }
+            this.addToLog({turn: 'Monster', text: 'Monster attack('+ attackPoint +')'});
+
+        },
+        addToLog: function(log) {
+            this.logs.push(log);
+        },
     },
     watch: {
         playerHealth: function(value) {
@@ -40,6 +52,10 @@ new Vue({
                 if(confirm('You lost the game. Wanna retry?')) {
                     this.playerHealth = 100;
                     this.monsterHealth = 100;
+                } else {
+                    this.playerHealth = 100;
+                    this.monsterHealth = 100;
+                    this.gameIsOn = false;
                 }
 
             } else if(value >= 100) {
@@ -53,6 +69,10 @@ new Vue({
                 if(confirm('You won the game. Wanna retry?')) {
                     this.playerHealth = 100;
                     this.monsterHealth = 100;
+                } else {
+                    this.playerHealth = 100;
+                    this.monsterHealth = 100;
+                    this.gameIsOn = false;
                 }
             }
         }
