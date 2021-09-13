d5112dda5f5ca0040b36762eeb1ef486232321ea
toghrulhajiyev <togrul.haciyev24@gmail.com>
2021-09-13 22:33:22 +0400

    Action buttons coded

--- js/app.js
+++ js/app.js
@@ -10,16 +10,26 @@ new Vue({
             this.gameIsOn = true;
         },
         attack: function() {
-
+            let attackPoint = Math.ceil(Math.random() * 10);
+            this.monsterHealth -= attackPoint;
+            this.monsterAttack();
         },
         specialAttack: function() {
-
+            let attackPoint = Math.ceil(Math.random() * 25);
+            this.monsterHealth -= attackPoint;
+            this.monsterAttack();
         },
         healUp: function() {
-
+            let healPoint = Math.ceil(Math.random() * 20);
+            this.playerHealth += healPoint;
+            this.monsterAttack();
         },
         giveUp: function() {
-
+            this.playerHealth = 0;
         },
+        monsterAttack: function() {
+            let attackPoint = Math.ceil(Math.random() * 15);
+            this.playerHealth -= attackPoint;
+        }
     }
 })
