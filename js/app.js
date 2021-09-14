de855d7e858a6218cb5b92a3764363b6df20d6df
Toghrul Hajiyev <togrul.haciyev@azerimed.com>
2021-09-14 16:07:19 +0400

    Code optimized (project finished)

--- js/app.js
+++ js/app.js
@@ -5,39 +5,45 @@ new Vue({
         monsterHealth: 100,
         gameIsOn: false,
         logs: [],
+        points: {
+            player_attack: 10,
+            player_special_attack: 25,
+            player_heal_up: 20,
+            monster_attack: 15
+        },
+        logTexts: {
+            atk_text: 'Player attack',
+            special_atk: 'Player special attack',
+            heal_up: 'Player healed up',
+            give_up: 'Player gave up',
+            monster_atk: 'Monster attack'
+        },
     },
     methods: {
         newGame: function() {
             this.gameIsOn = true;
         },
-        attack: function() {
-            let attackPoint = Math.ceil(Math.random() * 10);
+        playerAttack: function(atkPoint, atkText) {
+            let attackPoint = Math.ceil(Math.random() * atkPoint);
             this.monsterHealth -= attackPoint;
-            this.addToLog({turn: 'Player', text: 'Player attack('+ attackPoint +')'});
-            this.monsterAttack();
-        },
-        specialAttack: function() {
-            let attackPoint = Math.ceil(Math.random() * 25);
-            this.monsterHealth -= attackPoint;
-            this.addToLog({turn: 'Player', text: 'Special player attack('+ attackPoint + ')'});
-
+            this.addToLog({turn: 'Player', text: atkText + '(' + attackPoint + ')'});
             this.monsterAttack();
         },
         healUp: function() {
-            let healPoint = Math.ceil(Math.random() * 20);
+            let healPoint = Math.ceil(Math.random() * this.points.player_heal_up);
             this.playerHealth += healPoint;
-            this.addToLog({turn: 'Player', text: 'Player healed up('+ healPoint +')'});
+            this.addToLog({turn: 'Player', text: this.logTexts.heal_up + '(' + healPoint + ')'});
 
             this.monsterAttack();
         },
         giveUp: function() {
             this.playerHealth = 0;
-            this.addToLog({turn: 'Player', text: 'Player gave up'});
+            this.addToLog({turn: 'Player', text: this.give_up});
         },
         monsterAttack: function() {
-            let attackPoint = Math.ceil(Math.random() * 15);
+            let attackPoint = Math.ceil(Math.random() * this.points.monster_attack);
             this.playerHealth -= attackPoint;
-            this.addToLog({turn: 'Monster', text: 'Monster attack('+ attackPoint +')'});
+            this.addToLog({turn: 'Monster', text: this.logTexts.monster_atk + '(' + attackPoint + ')'});
 
         },
         addToLog: function(log) {
@@ -85,6 +91,18 @@ new Vue({
                     this.clearLogs(this.logs);
 
                 }
+            }
+        }
+    },
+    computed: {
+        userProgress: function() {
+            return {
+                width: this.playerHealth + '%',
+            }
+        },
+        monsterProgress: function() {
+            return {
+                width: this.monsterHealth + '%',
             }
         }
     }
